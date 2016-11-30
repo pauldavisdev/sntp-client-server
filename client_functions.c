382f00d6233a4ddbf9beea6b1883ffe0d4e4839e
P-Davis <pauldavis4@gmail.com>
2016-11-30 14:24:27 +0000

    Add client specific functions to seperate files

--- client_functions.c
+++ client_functions.c
@@ -15,3 +15,27 @@ void set_client_flags(ntp_packet *p)
   // mode
   p->flags |= 3;
 }
+
+double ntp_to_double(ntp_timestamp *p)
+{
+  double t1_s = (p->second);
+  double t1_f = (p->fraction) / (double)(1LL<<32);
+  t1_s += t1_f;
+  return t1_s;
+}
+
+double calculate_offset(ntp_packet *p, ntp_timestamp *t)
+{
+  double offset = (((ntp_to_double(&p->recvTimestamp)) - ntp_to_double(&p->orgTimestamp)) +
+                  ((ntp_to_double(&p->transmitTimestamp)) - ntp_to_double(t))) / 2;
+
+  return offset;
+}
+
+double calculate_delay(ntp_packet *p, ntp_timestamp *t)
+{
+  double delay = (((ntp_to_double(t) - ntp_to_double(&p->orgTimestamp)) ) -
+                 ((ntp_to_double(&p->transmitTimestamp) - ntp_to_double(&p->recvTimestamp))));
+
+  return delay;
+}
--- client_functions.h
+++ client_functions.h
@@ -1,1 +1,7 @@
 void set_client_flags(ntp_packet *p);
+
+double ntp_to_double(ntp_timestamp *p);
+
+double calculate_offset(ntp_packet *p, ntp_timestamp *t);
+
+double calculate_delay(ntp_packet *p, ntp_timestamp *t);
