5cbdcad7c7355671faaf8bf37d8fe21c65cbb02b
P-Davis <pauldavis4@gmail.com>
2016-12-08 12:53:26 +0000

    Add function description

--- server_functions.c
+++ server_functions.c
@@ -2,6 +2,15 @@
 
 #include "sntp.h"
 
+/*  Function:     set_server_reply
+*   Description:  Converts the client request packet from network to host byte
+*   order. Then sets all fields that are needed for a reply to unicast client.
+*   Finally converts the server reply packet from host to network byte order.
+*
+*   Parameters:   ntp_packet*
+*
+*   Returns:      none
+*/
 void set_server_reply(ntp_packet *p)
 {
   network_to_host(p);
